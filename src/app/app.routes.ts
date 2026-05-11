7ab7a740b23d15f1317f95a251c16cc08a3ad91f
Kiim94 <nemomoa@hotmail.com>
2026-05-11 09:15:13 +0200

    Tog bort katt-tabellen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { Courses } from "./courses/courses";
-import { Cats } from "./cats/cats";
 
 export const routes: Routes = [
     { path: 'courses', component: Courses},
-    {path: "cats", component: Cats},
     { path: '', redirectTo: 'courses', pathMatch: 'full' }
 ];
