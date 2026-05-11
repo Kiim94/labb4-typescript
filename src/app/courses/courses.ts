8a7ac7aeb3e1563c670824ed48e4f9e4313ec096
Kiim94 <nemomoa@hotmail.com>
2026-05-11 10:13:09 +0200

    Uppdaterat med kommentarer

--- src/app/courses/courses.ts
+++ src/app/courses/courses.ts
@@ -1,4 +1,6 @@
 import { Component, signal, computed } from '@angular/core';
+
+//importerar service från course. Där finns också interface för kurser 
 import { CourseService, Course } from "../services/course";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
@@ -19,30 +21,45 @@ export class Courses {
   //för sortering av kolumner. Utgår initialt från första kolumnen "code", a-ö. 
   sortCourses = signal<keyof Course>("code");
 
+  //sortering: grundläga = true, mao stigande (A-Ö)
   sortAsc= signal<boolean>(true);
 
   //hade problem med att se tabell. Använde denna nedan för att se om ngt ändrades
   loading= signal<boolean>(true);
 
   constructor(private courseService: CourseService){
+    //när data har kommit från courseService/från getCourses(), uppdateras signals (courses, loading)
     this.courseService.getCourses().subscribe(data => {
+      //tom courses får nu data
       this.courses.set(data);
+      //loading blir false, då tabellen borde vara färdigladdad/fått information
       this.loading.set(false);
     });
   }
 
+  //metod som ska räkna ut automatiskt:
   filteredCourses = computed(() => {
-    const phrase = this.searchPhrase().toLowerCase();
+    //tar det användaren söker och gör till små bokstäver, trimmar bort mellanslag före och efter
+    const phrase = this.searchPhrase().toLowerCase().trim();
 
+    //filtera igenom arrayen courses som har data från json
+    //filtrering jämför sökfras mot kursobjekten
     let result = this.courses().filter(c =>
       c.code.toLowerCase().includes(phrase) ||
       c.coursename.toLowerCase().includes(phrase) ||
       c.progression.toLowerCase().includes(phrase)
     );
 
+    //hämta nuvarande nyckel som sorteras efter (code, coursename, progression)
     const key = this.sortCourses();
+    //hämta nuvarande "sorteringsriktning" (är det stigande eller fallande, true eller false?)
     const asc = this.sortAsc();
+
+    //returnera resultat: sortera
     return result.sort((a,b) => {
+      //gör två värden som ska jämföras mot varandra: key kan vara code, coursename eller progression:
+      //String(a[code]).toLowerCase()
+      //Betyder ungefär: ta ut ett värde ur objektet (t.ex. code), gör till sträng och sedan till små bokstäver
       const aValue = String(a[key]).toLowerCase();
       const bValue = String(b[key]).toLowerCase();
 
@@ -55,13 +72,22 @@ export class Courses {
     });
 });
 
+  //uppdatera searchPhrases signal så den har det nya värdet
+  //t.ex. användaren söker på "javascript". Nytt värde är javascript
   setSearch(value: string){
     this.searchPhrase.set(value);
   }
+
+  //sortera tabell efter den kolumn användaren klickade på
   sortBy(field: keyof Course) {
+    //är tabellen redan sorteras efter vald kolumn?
     if (this.sortCourses() === field) {
+      //om ja, ändra ordning: om det är true, blir false.
+      //A-ö, Ö-A
       this.sortAsc.update(v => !v);
     } else {
+      //om nej, ändra vilken kolumn som sorteras efter
+      //börja från A-ö
       this.sortCourses.set(field);
       this.sortAsc.set(true);
     }
--- src/app/services/course.ts
+++ src/app/services/course.ts
@@ -1,23 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+//Observable: data som kommer senare/asynkront från API
 import { Observable } from "rxjs";
 
+//utseende för hur varje kursobjekt ska se ut
 export interface Course {
   code: string;
   coursename: string;
   progression: string;
+  //använde inte syllabus i slutprodukten, men kan vara bra att behålla
   syllabus?: string;
 }
 
 @Injectable({
+  //providedIn: root <- gör servicen (api:t) tillgängligt i hela webbplatsen
   providedIn: 'root',
 })
 export class CourseService {
+  //URL till json med kurser
   private url = "https://webbutveckling.miun.se/files/ramschema.json";
 
   constructor(private http: HttpClient){}
 
+  //Observable<Course[]> <- hämtar kurser från länk ovan. Observable gör den hämtningen "lazy"
   getCourses(): Observable<Course[]>{
+    //HTTP GET-anrop till api
     return this.http.get<Course[]>(this.url);
   }
 }
